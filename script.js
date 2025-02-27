e7486456f6d83748923b89661a436583e42836e5
Ayesha Ashraf <ayeshaashraf0999@gmail.com>
2025-02-27 04:07:18 -0700

    typetwoAdded

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelector(".video-placeholder").addEventListener("click", function () {
+    document.querySelector(".custom-alert").style.display = "block";
+  });
+  
+  document.querySelector(".close-alert").addEventListener("click", function () {
+    document.querySelector(".custom-alert").style.display = "none";
+  });
+
